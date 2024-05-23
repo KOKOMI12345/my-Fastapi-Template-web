4387cc6e0eef78371568966c395b467a2341bd87
nanak <2152598815@qq.com>
2024-05-23 17:52:01 +0800

    update

--- src/service/requent/index.ts
+++ src/service/requent/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import {useMessage} from 'naive-ui';
 import {useAuthStore} from "@/store/modules/auth";
 
 
@@ -27,8 +26,6 @@ service.interceptors.response.use(
 	response => {
 		const res = response.data
 		if (res.code !== import.meta.env.VITE_SERVICE_SUCCESS_CODE) {
-			const message = useMessage()
-			message.error(res.msg)
 			return Promise.reject(new Error(res.msg))
 		} else {
 			return response.data
@@ -36,8 +33,6 @@ service.interceptors.response.use(
 	},
 	error => {
 		console.log('err' + error)
-		const message = useMessage()
-		message.error("err" + error)
 		return Promise.reject(error)
 	})
 
